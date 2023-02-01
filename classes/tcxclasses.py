833ff47cc28c7f20f02159f7e29a295a4146e7a4
Paolo Bonomi <paolo.bonomi@outlook.com>
2023-02-01 17:52:35 +0100

    WIP

---
+++ classes/tcxclasses.py
@@ -0,0 +1,68 @@
+from bmath.mathbike import iterVel
+from parser.tcx import getData
+
+class TrackPoint:
+
+    def __init__(self):
+        self.speed = 0
+        self.cadence = 0
+        self.dist = 0;
+
+    def update(self, marker, value):
+        if( marker == "ns3:Watts" ):
+            self.speed = iterVel(int(value), 20/3.6)
+        if ( marker == "Cadence"):
+            self.cadence = int(value);
+
+class Lap:
+
+    def __init__(self):
+        self.tps = [TrackPoint()];
+        self.tot_dist = 0;
+        self.max_speed = 0
+        self.avg_speed = 0
+        self.avg_cad = 0
+        self.count = 0
+
+    def update(self, marker, line):
+        if ( marker == "Trackpoint" ):
+            self.tps.append(TrackPoint())
+        else:
+            self.tps[-1].update(marker, getData(line, marker))
+
+    def finish(self):
+        for tr in self.tps[1:]:
+            if tr.speed > self.max_speed:
+                self.max_speed = tr.speed
+
+            self.tot_dist += tr.speed
+            self.count = self.count + 1
+            self.avg_cad += tr.cadence
+            self.avg_speed += tr.speed
+
+        self.avg_speed /= self.count
+        self.avg_cad /= self.count
+    
+    def getTrackPoint(self, index):
+        return self.tps[index+1];
+
+class Course:
+
+    def __init__(self):
+
+        self.laps = [Lap()];
+
+    def update(self, marker, line):
+        if ( marker[0:3] == "Lap"):
+            self.laps.append(Lap());
+        else:
+            self.laps[-1].update(marker, line)
+
+    def finish(self):
+        print(self.laps[1:])
+        for lp in self.laps[1:]:
+            lp.finish();
+
+    def getLap(self, index):
+        return self.laps[index+1];
+
--- main.py
+++ main.py
@@ -1,76 +1,65 @@
 import sys
 
-from bmath.mathbike import iterVel
-from parser.tcx import getData
 from parser.tcx import getMarker
+from classes.tcxclasses import Course
 
-class TrackPoint:
-    speed = 0
-    cadence = 0
-
-    def update(self, line, marker):
-        if( marker == "ns3:Watts" ):
-            self.speed = iterVel(int(getData(line, marker)), 20/3.6)
-        if ( marker == "Cadence"):
-            self.cadence = int(getData(line, marker))
-
-tot_dist = 0
-array = []
-array.append(TrackPoint())
+course = Course();
 
 #read
 with open(sys.argv[1], "r") as file_input:
     for line in file_input:
         marker = str(getMarker(line))
-        if( marker == "" ):
-            continue
-        if ( marker == "Trackpoint" ):
-            array.append(TrackPoint())
-        else:
-            array[-1].update(line, marker)
+        if( marker != "" ):
+            course.update(marker, line)
 
-tot_dist = 0
-max_speed = 0
-avg_speed = 0
-avg_cad = 0
-count = -1
+course.finish();
 
-for tr in array:
-    if tr.speed > max_speed:
-        max_speed = tr.speed
+is_header = True;
 
-    tot_dist += tr.speed
-    count = count + 1
-    avg_cad += tr.cadence
-    avg_speed += tr.speed
+lap_count = 0;
+lap = course.getLap(0)
 
-avg_speed /= count
-avg_cad /= count
-
-h = True
-count = 0
+tp_count = -1;
+tp = 0;
 
 #write
 name = sys.argv[1]
 name = name[:-4]
 with open(sys.argv[1], "r") as file_input:
     with open(name+"_fixed.tcx", "w") as file_output:
-        distance = 0
-        for line in file_input:
-            if line.lstrip().startswith("<Cadence>") and h:
-                line = "<Cadence>"+str(avg_cad)+"</Cadence>"
-            if line.lstrip().startswith("<Trackpoint>"):
-                h = False
-                count = count + 1
-            if line.lstrip().startswith("<DistanceMeters>") and h: #header
-                line = "<DistanceMeters>"+str(tot_dist)+"</DistanceMeters>"
-            if line.lstrip().startswith("<MaximumSpeed>") and h:
-                line = "<MaximumSpeed>"+str(max_speed)+"</MaximumSpeed>"
-            if line.lstrip().startswith("<DistanceMeters>") and not h: #TrackPoint
-                distance += array[count].speed
-                line = "<DistanceMeters>"+str(distance)+"</DistanceMeters>"
-            if line.lstrip().startswith("<ns3:Speed>") and not h:
-                line = "<ns3:Speed>"+str(array[count].speed)+"</ns3:Speed>"
-            if line.lstrip().startswith("<ns3:AvgSpeed>"):
-                line = "<ns3:AvgSpeed>"+str(avg_speed)+"</ns3:AvgSpeed>"
+        for line in file_input:    
+            if is_header :
+                if line.lstrip().startswith("<Trackpoint>"):
+                    is_header = False
+                    tp_count = tp_count + 1
+                    tp = lap.getTrackPoint(tp_count);
+
+                if line.lstrip().startswith("<Cadence>"):
+                    print(line);
+                    line = "<Cadence>"+str(lap.avg_cad)+"</Cadence>"
+                    print(line);
+
+                if line.lstrip().startswith("<DistanceMeters>"):
+                    
+                    line = "<DistanceMeters>"+str(lap.tot_dist)+"</DistanceMeters>"
+                
+                if line.lstrip().startswith("<MaximumSpeed>"):
+                    line = "<MaximumSpeed>"+str(lap.max_speed)+"</MaximumSpeed>"
+            else:
+                if line.lstrip().startswith("<Lap StartTime"):
+                    lap_count = lap_count + 1;
+                    lap = course.getLap(lap_count);
+                    is_header = True;
+                    tp_count = -1;
+
+                if line.lstrip().startswith("<DistanceMeters>"): #TrackPoint
+                    tp.dist += tp.speed
+                    line = "<DistanceMeters>"+str(tp.dist)+"</DistanceMeters>"
+                
+                if line.lstrip().startswith("<ns3:Speed>"):
+                    line = "<ns3:Speed>"+str(tp.speed)+"</ns3:Speed>"
+                
+                if line.lstrip().startswith("<ns3:AvgSpeed>"):
+                    line = "<ns3:AvgSpeed>"+str(lap.avg_speed)+"</ns3:AvgSpeed>"
+            
             file_output.write(line)
